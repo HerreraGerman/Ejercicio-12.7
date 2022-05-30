b83b15037a6a757470e58c4fb09fa9711cc0955a
GerN7 <brither5478@gmail.com>
2022-05-30 18:40:31 -0300

    Remodelacion del ejercicio

---
+++ Ejercicio12.7/TablaDispersaEnlazada.java
@@ -0,0 +1,79 @@
+package Ejercicio127;
+
+public class TablaDispersaEnlazada{
+    static final int M = 101;
+    private Elemento [] tabla;
+    private int numElementos;
+    
+    // operaciones
+
+    static final double R = 0.618034;
+    static int dispersion(long x){
+        double t;
+        int v;
+        t = R * x - Math.floor(R * x); // parte decimal
+        v = (int) (M * t);
+        return v;
+    }
+
+    public TablaDispersaEnlazada(){ // constructor
+        tabla = new Elemento[M];
+        for (int k = 0; k < M; k++)
+        tabla[k] = null;
+        numElementos = 0;
+    }
+
+    public void insertar(TipoSocio s){
+        int posicion;
+        Elemento nuevo;
+        posicion = dispersion(s.getCodigo());
+        nuevo = new Elemento(s);
+        nuevo.sgte = tabla[posicion];
+        tabla[posicion] = nuevo;
+        numElementos++;
+    }
+
+    boolean conforme(TipoSocio a){
+        return conforme(a);
+    }
+
+    public void eliminar(int codigo){
+        int posicion;
+        posicion = dispersion(codigo);
+        if (tabla[posicion] != null){
+            // no está vacía
+            Elemento anterior, actual;
+            anterior = null;
+            actual = tabla[posicion];
+            while ((actual.sgte != null) && actual.getSocio().getCodigo() != codigo){
+                anterior = actual;
+                actual = actual.sgte;
+            }
+            if (actual.getSocio().getCodigo() != codigo)
+                System.out.println("No se encuentra en la tabla el socio " + codigo);
+            else if (conforme (actual.getSocio())){
+            //se retira el nodo
+            if (anterior == null) // primer nodo
+                tabla[posicion] = actual.sgte;
+            else
+                anterior.sgte = actual.sgte;
+            actual = null;
+            numElementos--;
+            }
+        }
+    }
+
+    public Elemento buscar(int codigo){
+        Elemento p = null;
+        int posicion;
+        posicion = dispersion(codigo);
+        if (tabla[posicion] != null){
+            p = tabla[posicion];
+            while ((p.sgte != null) && p.socio.codigo != codigo)
+                p = p.sgte;
+                if (p.socio.codigo != codigo)
+                    p = null;
+        }
+        return p;
+    }
+}
